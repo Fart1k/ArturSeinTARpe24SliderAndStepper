db82e54ae158a42f8c98696f5779d1a0eceae5b1
opilane <opilane@HP-CZC2349HTN>
2026-03-04 11:10:30 +0200

    Lumemme pos

--- SliderAndStepper/SnowManPage.xaml.cs
+++ SliderAndStepper/SnowManPage.xaml.cs
@@ -1,4 +1,7 @@
+
+using Microsoft.Maui.Controls;
 using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Layouts;
 
 namespace SliderAndStepper;
 
@@ -11,8 +14,60 @@ public partial class SnowManPage : ContentPage
     Ellipse bodyOne;
 	Ellipse bodyTwo;
 
+	AbsoluteLayout al;
 	public SnowManPage()
 	{
-		
-	}
+		hat = new Frame
+		{
+			WidthRequest = 40,
+			HeightRequest = 50,
+			BorderColor = Colors.Black,
+			BackgroundColor = Colors.LightGray
+		};
+		head = new Ellipse
+		{
+			WidthRequest = 75,
+			HeightRequest = 75,
+			Fill = Colors.White,
+			Stroke = Colors.Black,
+			StrokeThickness = 2,
+		};
+		bodyOne = new Ellipse
+		{
+			WidthRequest = 100,
+			HeightRequest = 100,
+            Fill = Colors.White,
+            Stroke = Colors.Black,
+            StrokeThickness = 2,
+        };
+		bodyTwo = new Ellipse
+		{
+			WidthRequest = 125,
+			HeightRequest = 125,
+            Fill = Colors.White,
+            Stroke = Colors.Black,
+            StrokeThickness = 2,
+        };
+
+		al = new AbsoluteLayout
+		{
+			HeightRequest = 400,
+			Children = { hat, head, bodyOne, bodyTwo }
+		};
+
+        AbsoluteLayout.SetLayoutFlags(bodyTwo, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(bodyTwo, new Rect(0.5, 0.95, 125, 125));
+
+        AbsoluteLayout.SetLayoutFlags(bodyOne, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(bodyOne, new Rect(0.5, 0.55, 100, 100));
+
+        AbsoluteLayout.SetLayoutFlags(head, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(head, new Rect(0.5, 0.3, 75, 75));
+
+        AbsoluteLayout.SetLayoutFlags(hat, AbsoluteLayoutFlags.PositionProportional);
+        AbsoluteLayout.SetLayoutBounds(hat, new Rect(0.5, 0.15, 50, 40));
+
+        Content = al;
+
+    }
 }
