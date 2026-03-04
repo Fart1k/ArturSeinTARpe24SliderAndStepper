fd66345251435e66c3b1378e723d8d0274292103
opilane <opilane@HP-CZC2349HTN>
2026-03-04 10:41:40 +0200

    awd

--- SliderAndStepper/SnowManPage.xaml.cs
+++ SliderAndStepper/SnowManPage.xaml.cs
@@ -1,9 +1,18 @@
+using Microsoft.Maui.Controls.Shapes;
+
 namespace SliderAndStepper;
 
 public partial class SnowManPage : ContentPage
 {
+	// Snowman
+
+	Frame hat;
+    Ellipse head;
+    Ellipse bodyOne;
+	Ellipse bodyTwo;
+
 	public SnowManPage()
 	{
-		InitializeComponent();
+		
 	}
 }
